a6447cb0b200805eaa50bc91f7a294147c83a8c4
ChangRuXuan <1359290997@qq.com>
2020-12-10 17:30:57 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+
+        System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");  
     }
 }
