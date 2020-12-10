ef9d8c95840d24ba6cd0d93cc56274660061e62c
ChangRuXuan <1359290997@qq.com>
2020-12-10 17:48:28 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
+        System.out.println("sdsad");
         System.out.println("HelloWorld!");
 
         System.out.println("HelloWorld!");
